3f1951ded1a05e1b5e4079eab05c3d1de352b680
marco-milani <marco.milani1999@gmail.com>
2022-06-06 22:07:41 +0200

    base for server

---
+++ server/dao.js
@@ -0,0 +1,233 @@
+'use strict';
+/* Data Access Object (DAO) module for accessing exams */
+
+const sqlite = require('sqlite3');
+const {Exam} = require('./exam');
+const dayjs = require("dayjs");
+const { resolve } = require('path');
+
+const db = new sqlite.Database('studyPlan.db', err => {
+  if (err) throw err;
+});
+
+exports.listAllExam = () => {
+  return new Promise((resolve, reject) => {
+    const sql = 'SELECT * FROM exam';
+
+    db.all(sql, (err, rows) => {
+      if (err) reject(err);
+      else {
+        const exams = rows.map(row =>
+            new Exam(
+                row.code,
+                row.name,
+                row.credits,
+                row.max,
+                row.preparation,
+                row.description
+            ))
+        resolve(exams);
+      }
+    })
+  });
+
+}
+
+
+
+exports.getExamByCode = (code) => {
+  return new Promise((resolve, reject) => {
+    const sql = 'SELECT * FROM exam WHERE code=?';
+    db.get(sql, id, (err, row) => {
+      if (err) reject(err);
+      else {
+        if(row) {
+          const exam = new Exam(
+            row.code,
+            row.name,
+            row.credits,
+            row.max,
+            row.preparation,
+            row.description
+          )
+          resolve(exam);
+        }
+        else {
+          reject(row);
+        }
+      }
+    })
+  });
+
+}
+
+exports.addExamPlan = (exam,userId) => {
+  return new Promise((resolve, reject) => {
+        const sql = "INSERT INTO planExam(id,code) VALUES (?, ?)";
+        const sql2="SELECT id from plan WHERE userId=?";
+        const sql3= "UPDATE plan SET credits= credits + ? WHERE id=?"
+        let planId;
+        db.get(sql2, userId, (err, row) => {//prendo plan id
+          if (err) reject(err);
+          else {
+            if(row) {
+              planId=row.id;
+            }
+            resolve(planId);
+          }
+        });
+
+        db.run(sql, [planId,exam.code], (err) => {// inserisco esame dentro piano di studio
+          if (err)
+            reject(err);
+          else
+            resolve(this.lastID);
+        });
+        db.run(sql3, [exam.credits,planId], (err) => { //aggiorno piano distudio con crediti dell'esame
+          if (err)
+            reject(err);
+          else
+            resolve(this.lastID);
+        });
+      }
+  );
+
+}//new plan(set type)
+
+exports.NewPlan = (type,userId) => {
+  return new Promise((resolve, reject) => {
+    const sql = "INSERT INTO plan(type,credits,userId) VALUES(?,?,?)";
+    db.run(sql, [type,userId,0], (err) => {
+      if (err)
+        reject(err);
+      else
+         
+        resolve(this.lastID)
+    })
+  });
+}
+
+//delete exam from plan
+
+exports.deleteExamPlan = (exam,userId) => {
+  return new Promise((resolve, reject) => {
+        const sql = "DELETE FROM planExam WHERE code=? and id=?";
+        const sql2="SELECT id from plan WHERE userId=?";
+        const sql3= "UPDATE plan SET credits= credits - ? WHERE id=?"
+        let planId;
+        db.get(sql2, userId, (err, row) => {//prendo plan id
+          if (err) reject(err);
+          else {
+            if(row) {
+              planId=row.id;
+            }
+            resolve(planId);
+          }
+        });
+
+        db.run(sql, [planId,exam.code], (err) => {// elimino esame dentro piano di studio
+          if (err)
+            reject(err);
+          else
+            resolve(this.lastID);
+        });
+        db.run(sql3, [exam.credits,planId], (err) => { //aggiorno piano distudio con crediti dell'esame
+          if (err)
+            reject(err);
+          else
+            resolve(this.lastID);
+        });
+      }
+  );
+}
+exports.deletePlan=(id)=>{
+  return new Promise((resolve,reject)=>{
+    const sql="DELETE FROM plan WHERE id=?";
+    db.run(sql, [id], (err) => {// elimino plan 
+      if (err)
+        reject(err);
+      else
+        resolve(this.lastID);
+    });
+  })
+}
+
+exports.login = (film,user) => {
+  return new Promise((resolve, reject) => {
+    const sql = "SELECT hash FROM users WHERE email=?";
+    db.get(sql, [user.email], (err) => {
+      if (err)
+        reject(err);
+      else{
+          // TODO: resolve
+          if(row) {
+            let pwd=row.hash;
+            //check pwd hash
+          }
+          resolve(planId);
+        }
+    })
+  });
+}
+
+exports.addExam=(exam) =>{
+  return new Promise((resolve,reject)=> {
+    const sql="INSERT INTO exam(code,name,credits,max,preparation,description) VALUES(?,?,?,?,?,?)";
+    db.run(sql,[exam.code,exam.name,exam.credits,exam.max,exam.preparation,exam.descriptiom],(err)=>{
+      if(err)
+        reject(err);
+        else{
+          resolve(this.lastID)
+        }
+    })
+  })
+}
+
+exports.addDefaultExam=()=>{
+  let exam=new Exam("02GOLOV","Architetture dei sistemi di elaborazione",12,null,null,null);
+  await this.addExam(exam);
+  let exam1=new Exam("02LSEOV","Computer architectures",12,null,null,null);
+  await this.addExam(exam1);
+  let exam2=new Exam("01SQJOV","Data Science and Database Technology",8,null,null,null);
+  await this.addExam(exam2);
+  let exam3=new Exam("01SQMOV","Data Science e Tecnologie per le Basi di Dati",8,null,null,null);
+  await this.addExam(exam3);
+  let exam4=new Exam("01SQLOV","Database systems",8,null,null,null);
+  await this.addExam(exam4);
+  let exam5=new Exam("01OTWOV","Computer network technologies and services",6,3,null,null);
+  await this.addExam(exam5);
+  let exam6=new Exam("02KPNOV","Tecnologie e servizi di rete",6,3,null,null);
+  await this.addExam(exam6);
+  let exam7=new Exam("01TYMOV","Information systems security services",12,null,null,null);
+  await this.addExam(exam7);
+  let exam8=new Exam("01UDUOV","Sicurezza dei sistemi informativi",12,null,null,null);
+  await this.addExam(exam8);
+  let exam9=new Exam("05BIDOV","Ingegneria del software",6,null,"02GOLOV",null);
+  await this.addExam(exam9);
+  let exam10=new Exam("04GSPOV","Software engineering",6,null,"02LSEOV",null);
+  await this.addExam(exam10);
+  let exam11=new Exam("01UDFOV","Applicazioni Web I",6,null,null,null);
+  await this.addExam(exam11);
+  let exam12=new Exam("01TXYOV","Web Applications I",6,3,null,null);
+  await this.addExam(exam12);
+  let exam13=new Exam("01TXSOV","Web Applications II",6,null,"01TXYOV",null);
+  await this.addExam(exam13);
+  let exam14=new Exam("02GRSOV","Programmazione di sistema",6,null,null,null);
+  await this.addExam(exam14);
+  let exam15=new Exam("01NYHOV","System and device programming",6,3,null,null);
+  await this.addExam(exam15);
+  let exam16=new Exam("01SQOOV","Reti Locali e Data Center",6,null,null,null);
+  await this.addExam(exam16);
+  let exam17=new Exam("01TYDOV","Software networking",7,null,null,null);
+  await this.addExam(exam17);
+  let exam18=new Exam("03UEWOV","Challenge",5,null,null,null);
+  await this.addExam(exam18);
+  let exam19=new Exam("01URROV","Computational intelligence",6,null,null,null);
+  await this.addExam(exam19);
+  let exam20=new Exam("01OUZPD","Model based software design",4,null,null,null);
+  await this.addExam(exam20);
+  let exam21=new Exam("01URSPD","Internet Video Streaming",6,2,null,null);
+  await this.addExam(exam21);
+
+
+}
--- server/index.js
+++ server/index.js
@@ -1,10 +1,123 @@
 'use strict';
 
 const express = require('express');
+const res = require('express/lib/response');
+const morgan = require('morgan');
+const {check, validationResult} = require("express-validator");
+const dao = require('./dao');
+const PORT = 3001;
+const cors = require('cors');
+
 
 // init express
 const app = new express();
 const port = 3001;
+
+app.use(express.json());
+const corsOptions = {
+  origin: 'http://localhost:3000',
+  optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions));
+// GET APIs //
+
+// Get list of all exams //
+app.get('/api/exams', async (req, res) => {
+  try {
+    let list = await dao.listAllExam();
+    return res.status(200).json(list).end();
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({error: `Internal Server Error`}).end();
+  }
+});
+
+
+// Get exam element given its id //
+app.get('/api/exams/:id',
+    async (req, res) => {
+      const code = req.params.code;
+      try {
+        let exam = await dao.getExamByCode(code);
+        if (exam)
+          return res.status(200).json(exam).end();
+        else
+          return res.status(404).json({error: `Exam Code ${code} not found.`}).end();
+      } catch (err) {
+        console.log(err);
+        return res.status(500).json({error: `Internal Server Error`}).end();
+      }
+    });
+
+
+    //TO DO login 
+
+// POST APIs //
+
+// Create a new study plan //
+app.post('/api/plan',
+    [check("type").exists(),
+      check("userId").exists().isNumeric()],
+    async (req, res) => {
+      const errors = validationResult(req);
+      console.log(errors);
+      if (!errors.isEmpty()) {
+        return res.status(422).json({error: ' Validation of request body failed '}).end()
+      }
+      try {
+        await dao.NewPlan(req.body.type,req.body.userId);
+        return res.status(201).json("201 created").end();
+      } catch (err) {
+        console.log(err);
+        return res.status(500).json({error: `Internal Server Error`}).end();
+      }
+    });
+
+// PUT APIs //
+//TO DO capire come voglio fare aggiungere esami-> uno per volta da qui o passare lista qui 
+/*app.put('/api/films/:id',
+    [check("title").exists(),
+      check("favorite").exists(),
+      check("watchdate").exists(),
+      check("rating").exists().isNumeric(),
+      check("user").exists()],
+    async (req, res) => {
+      
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(422).json({error: ' Validation of request body failed '}).end()
+      }
+      const id = req.params.id;
+      try {
+          await dao.updateFilm(req.body,id);
+        return res.status(200).json("OK").end();
+      } catch (err) {
+        console.log(err);
+        return res.status(500).json({error: `Internal Server Error`}).end();
+      }
+    });*/
+
+
+// DELETE APIs //
+
+// Delete a film given its id //
+app.delete('/api/plan/:id',
+    async (req, res) => {
+        const id = req.params.id;
+        try {
+        const result = await dao.deletePlan(id);
+        if(result)
+            return res.status(204).end();
+        else
+            return res.status(404).json({error: `PlanId ${id} not found.`}).end();
+      } catch (err) {
+        console.log(err);
+        return res.status(500).json({error: `Internal Server Error`}).end();
+      }
+    });
+
+//TO DO api delete exam from exam plan
 
 // activate the server
 app.listen(port, () => {
